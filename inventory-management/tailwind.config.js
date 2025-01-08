d6da1469c6136a60b862e927fccdec06f7171d68
Daniel ohana <every.society@gmail.com>
2025-01-08 17:14:57 +0200

    TS to JS

---
+++ inventory-management/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,tsx,mdx}",
+    "./components/**/*.{js,jsx,tsx,mdx}",
+    "./app/**/*.{js,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
